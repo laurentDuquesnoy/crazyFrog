c70eaac705349770b68e2e2b4655075d0cd479f6
Developer0108 <laurentduquesnoy5@gmail.com>
2021-11-02 17:00:18 +0100

    facts list added, still bugging with scrollview

--- app/src/main/java/com/frog/crazyfrog/MainActivity.kt
+++ app/src/main/java/com/frog/crazyfrog/MainActivity.kt
@@ -3,6 +3,7 @@ package com.frog.crazyfrog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.commit
 import com.frog.crazyfrog.databinding.ActivityMainBinding
@@ -13,10 +14,24 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.locateButton.setOnClickListener{handleClick()}
+        fillFacts()
+        binding.rootScoll.scrollTo(0, binding.rootScoll.top)
     }
 
     private fun handleClick(){
         val intent = Intent(this, Locator::class.java)
         startActivity(intent)
     }
+
+    private fun fillFacts(){
+        val facts = arrayOf(
+            getString(R.string.fact_one),
+            getString(R.string.fact_two),
+            getString(R.string.fact_three),
+            getString(R.string.fact_four)
+        )
+        val adapter = ArrayAdapter(this, R.layout.facts_list_item, facts)
+        binding.factList.adapter = adapter
+    }
+
 }
