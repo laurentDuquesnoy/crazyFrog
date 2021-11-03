06c9121ba153fcb00671382d89804a2721cc88a3
Developer0108 <laurentduquesnoy5@gmail.com>
2021-11-03 15:32:17 +0100

    sounboard done, app ready to deploy

--- app/src/main/java/com/frog/crazyfrog/Soundboard.kt
+++ app/src/main/java/com/frog/crazyfrog/Soundboard.kt
@@ -1,15 +1,16 @@
 package com.frog.crazyfrog
 
+import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import androidx.databinding.DataBindingUtil
+import com.frog.crazyfrog.databinding.FragmentSoundboardBinding
+import android.media.MediaPlayer
+import android.widget.ImageView
+import android.widget.LinearLayout
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +18,46 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class Soundboard : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private lateinit var binding : FragmentSoundboardBinding
+    private var mMediaPlayer : MediaPlayer? = null
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_soundboard, container, false)
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_soundboard, container, false)
+        setListeners()
+        return binding.root
     }
 
+    private fun setListeners(){
+        binding.buttonSoundOne.setOnClickListener { SoundBoardPlay("soundOne" , binding.imageView1, binding.buttonSoundOne) }
+        binding.buttonSoundTwo.setOnClickListener{SoundBoardPlay("soundTwo" , binding.imageView2, binding.buttonSoundTwo)}
+        binding.buttonSoundThree.setOnClickListener{SoundBoardPlay("soundThree", binding.imageView3, binding.buttonSoundThree)}
+
+    }
+
+    private fun SoundBoardPlay(sound : String , imageview : ImageView, linearlayout : LinearLayout){
+        when(sound){
+            "soundOne" -> mMediaPlayer = MediaPlayer.create(context, R.raw.dingding)
+            "soundTwo" -> mMediaPlayer = MediaPlayer.create(context, R.raw.axelf_remix)
+            "soundThree" -> mMediaPlayer = MediaPlayer.create(context, R.raw.crazy_frog_3)
+        }
+        mMediaPlayer!!.isLooping = false
+        mMediaPlayer!!.start()
+        imageview.setImageResource(R.drawable.custom_stopbutton)
+        linearlayout.setOnClickListener{mMediaPlayer!!.stop()
+            linearlayout.setOnClickListener{SoundBoardPlay(sound, imageview, linearlayout)}
+            imageview.setImageResource(R.drawable.custom_playbutton)}
+        mMediaPlayer!!.setOnCompletionListener { imageview.setImageResource(R.drawable.custom_playbutton)
+            linearlayout.setOnClickListener{SoundBoardPlay(sound, imageview, linearlayout)}}
+    }
     companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment Soundboard.
-         */
-        // TODO: Rename and change types and number of parameters
+
         @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            Soundboard().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+        fun newInstance() =
+            Soundboard()
     }
 }
